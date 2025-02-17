7e13bd8dd89cc067235f58ae6bf84d0e08d2682e
PedroHPG <pedrohpgoncalves@hotmail.com>
2025-02-17 12:00:16 -0300

    Added list of currencies to be used

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,37 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("---------Currency Converter---------");
+		System.out.println("List of Available Currencies: 1 - Real, 2 - Dollar, 3 - Euro, 4 - Yen, 5 - Other");
 		System.out.print("Enter the currency you have: ");
-		String currency1 = sc.nextLine();
+		int currency1 = sc.nextInt();
+		System.out.println("You have selected: " + showCurrency(currency1));
 		
 		System.out.print("Enter the currency you want: ");
-		String currency2 = sc.nextLine();
+		int currency2 = sc.nextInt();
+		System.out.println("You have selected: " + showCurrency(currency2));
 		
 		System.out.print("Enter the amount you have: ");
 		float value = sc.nextFloat();
 		
 		System.out.println();
 		System.out.println("That sure is some data you just entered");
+	}
+	
+	public static String showCurrency(int currency) {
+		String result = "";
+		switch (currency) {
+		case 1: result = "Real";
+				break;
+		case 2: result = "Dollar";
+				break;
+		case 3: result = "Euro";
+				break;
+		case 4: result = "Yen";
+				break;
+		default: System.out.println("We don't do monopoly money, sorry");
+				result = "Some fake money thing";
+				break;
+		}
+		return result;
 	}
 }
