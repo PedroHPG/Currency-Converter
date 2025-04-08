dff1c94dc5bcd328c979fdccbbc5b25f16cd8699
PedroHPG <pedrohpgoncalves@hotmail.com>
2025-04-08 11:11:52 -0300

    Added get/set/constructors, moved methods to class

--- src/main/java/Currency.java
+++ src/main/java/Currency.java
@@ -3,5 +3,49 @@ public class Currency {
 
 	private String name;
 	private String code;
-	private String conversionRate;
+	private float conversionRate;
+	
+	public Currency() {
+		
+	}
+	
+	public Currency(String name, String code, float conversionRate) {
+		super();
+		this.name = name;
+		this.code = code;
+		this.conversionRate = conversionRate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public float getConversionRate() {
+		return conversionRate;
+	}
+
+	public void setConversionRate(float conversionRate) {
+		this.conversionRate = conversionRate;
+	}
+	
+	public float convertToDollar(float value) {
+		return value/conversionRate;
+	}
+	
+	public float convertFromDollar(float value) {
+		return conversionRate*value;
+	}
+	
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,84 +30,28 @@ public class Main {
 		System.out.println("---------Currency Converter---------");
 		System.out.println("List of Available Currencies: 1 - Dollar, 2 - Real, 3 - Euro, 4 - Yen, 5 - Other");
 		System.out.print("Enter the currency you have: ");
-		int currency1 = sc.nextInt();
-		System.out.println("You have selected: " + showCurrency(currency1));
+		//int currency1 = sc.nextInt();
+		//System.out.println("You have selected: " + showCurrency(currency1));
 		
-		System.out.print("Enter the currency you want: ");
-		int currency2 = sc.nextInt();
-		System.out.println("You have selected: " + showCurrency(currency2));
+		//System.out.print("Enter the currency you want: ");
+		//int currency2 = sc.nextInt();
+		//System.out.println("You have selected: " + showCurrency(currency2));
 		
-		System.out.print("Enter the amount you have: ");
-		float value = sc.nextFloat();
+		//System.out.print("Enter the amount you have: ");
+		//float value = sc.nextFloat();
 		
-		float result = 1;
+		//float result = 1;
 		
-		if(currency1 == 1) {
-			result = convertFromDollar(currency2, value);
-		} else if(currency2 == 1){
-			result = convertToDollar(currency1, value);
-		} else {
-			float newValue = convertToDollar(currency1, value);
-			result = convertFromDollar(currency2, newValue);
-		}
+		//if(currency1 == 1) {
+		//	result = convertFromDollar(currency2, value);
+		//} else if(currency2 == 1){
+		//	result = convertToDollar(currency1, value);
+		//} else {
+		//	float newValue = convertToDollar(currency1, value);
+		//	result = convertFromDollar(currency2, newValue);
+		//}
 		
-		System.out.println();
-		System.out.println("Your value is worth: " + result);
-	}
-	
-	public static String showCurrency(int currency) {
-		String result = "";
-		switch (currency) {
-		case 1: result = "Dollar";
-				break;
-		case 2: result = "Real";
-				break;
-		case 3: result = "Euro";
-				break;
-		case 4: result = "Yen";
-				break;
-		default: System.out.println("We don't do monopoly money, sorry");
-				result = "Some fake money thing";
-				break;
-		}
-		return result;
-	}
-	
-	public static float convertToDollar(int currency, float value) {
-		float result = 1;
-		float conversion = 1;
-		switch (currency) {
-		case 1: conversion = dollar;
-				break;
-		case 2: conversion = real;
-				break;
-		case 3: conversion = euro;
-				break;
-		case 4: conversion = yen;
-				break;
-		default: System.out.println("We don't do monopoly money, sorry");
-				break;
-		}
-		result = value/conversion;
-		return result;
-	}
-	
-	public static float convertFromDollar(int currency, float value) {
-		float result = 1;
-		float conversion = 1;
-		switch (currency) {
-		case 1: conversion = dollar;
-				break;
-		case 2: conversion = real;
-				break;
-		case 3: conversion = euro;
-				break;
-		case 4: conversion = yen;
-				break;
-		default: System.out.println("We don't do monopoly money, sorry");
-				break;
-		}
-		result = conversion*value;
-		return result;
-	}
+		//System.out.println();
+		//System.out.println("Your value is worth: " + result);
+	}	
 }
