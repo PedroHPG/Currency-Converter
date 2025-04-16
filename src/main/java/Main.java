867f99d3af06c9a95b0ea9e701b61cab1e8969df
PedroHPG <pedrohpgoncalves@hotmail.com>
2025-04-16 11:50:23 -0300

    Started webscraping for the conversion values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,46 +10,51 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		// initializing the HTML Document page variable 
-		//Document doc; 
+		Document doc; 
 		 
-		//try { 
+		try { 
 			// fetching the target website 
-		//	doc = Jsoup.connect("https://www.exchange-rates.org/current-rates/usd").userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36") 
-		//			.header("Accept-Language", "*").get(); 
-		//} catch (IOException e) { 
-		//	throw new RuntimeException(e); 
-		//}
+			doc = Jsoup.connect("https://www.exchange-rates.org/current-rates/usd").userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36") 
+					.header("Accept-Language", "*").get(); 
+		} catch (IOException e) { 
+			sc.close();
+			throw new RuntimeException(e); 
+		}
 
 		
 		
-		System.out.println("---------Currency Converter---------");
-		System.out.println("List of Available Currencies: 1 - Dollar, 2 - Real, 3 - Euro, 4 - Yen, 5 - Other");
-		System.out.print("Enter the currency you have: ");
-		int selector = sc.nextInt();
-		Currency currency1 = selectCurrency(selector);
-		System.out.println("You have selected: " + currency1.getName());
-		
-		System.out.print("Enter the currency you want: ");
-		selector = sc.nextInt();
-		Currency currency2 = selectCurrency(selector);
-		System.out.println("You have selected: " + currency2.getName());
-		
-		System.out.print("Enter the amount you have: ");
-		float value = sc.nextFloat();
-		
-		float result = 1;
-		
-		if(currency1.getName() == "Dollar") {
-			result = currency2.convertFromDollar(value);
-		} else if(currency2.getName() == "Dollar"){
-			result = currency1.convertToDollar(value);
-		} else {
-			float newValue = currency1.convertToDollar(value);
-			result = currency2.convertFromDollar(newValue);
+		//System.out.println("---------Currency Converter---------");
+		//System.out.println("List of Available Currencies: 1 - Dollar, 2 - Real, 3 - Euro, 4 - Yen, 5 - Other");
+		//System.out.print("Enter the currency you have: ");
+		Elements tests = doc.select("a[href*=https://www.exchange-rates.org/converter/usd-bsd]");
+		for(Element test : tests) {
+			System.out.println(test.attr("title"));
 		}
+		//int selector = sc.nextInt();
+		//Currency currency1 = selectCurrency(selector);
+		//System.out.println("You have selected: " + currency1.getName());
 		
-		System.out.println();
-		System.out.println("Your value is worth: " + result);
+		//System.out.print("Enter the currency you want: ");
+		//selector = sc.nextInt();
+		//Currency currency2 = selectCurrency(selector);
+		//System.out.println("You have selected: " + currency2.getName());
+		
+		//System.out.print("Enter the amount you have: ");
+		//float value = sc.nextFloat();
+		
+		//float result = 1;
+		
+		//if(currency1.getName() == "Dollar") {
+		//	result = currency2.convertFromDollar(value);
+		//} else if(currency2.getName() == "Dollar"){
+		//	result = currency1.convertToDollar(value);
+		//} else {
+		//	float newValue = currency1.convertToDollar(value);
+		//	result = currency2.convertFromDollar(newValue);
+		//}
+		
+		//System.out.println();
+		//System.out.println("Your value is worth: " + result);
 		sc.close();
 	}	
 	
