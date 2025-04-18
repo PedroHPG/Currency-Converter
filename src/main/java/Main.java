98aa5e615bd4d56626be0c3b3b599f9bb70d62e7
PedroHPG <pedrohpgoncalves@hotmail.com>
2025-04-18 12:05:27 -0300

    Finished integrating webscraped conversion rates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,68 +9,68 @@ public class Main {
 			
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// initializing the HTML Document page variable 
-		Document doc; 
-		 
-		try { 
-			// fetching the target website 
-			doc = Jsoup.connect("https://www.exchange-rates.org/current-rates/usd").userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36") 
-					.header("Accept-Language", "*").get(); 
-		} catch (IOException e) { 
-			sc.close();
-			throw new RuntimeException(e); 
+				
+		
+		System.out.println("---------Currency Converter---------");
+		System.out.println("List of Available Currencies: 1 - Dollar, 2 - Real, 3 - Euro, 4 - Yen, 5 - Other");
+		System.out.print("Enter the currency you have: ");
+		
+		int selector = sc.nextInt();
+		Currency currency1 = selectCurrency(selector);
+		System.out.println("You have selected: " + currency1.getName());
+		
+		System.out.print("Enter the currency you want: ");
+		selector = sc.nextInt();
+		Currency currency2 = selectCurrency(selector);
+		System.out.println("You have selected: " + currency2.getName());
+		
+		System.out.print("Enter the amount you have: ");
+		float value = sc.nextFloat();
+		
+		float result = 1;
+		
+		if(currency1.getName() == "Dollar") {
+			result = currency2.convertFromDollar(value);
+		} else if(currency2.getName() == "Dollar"){
+			result = currency1.convertToDollar(value);
+		} else {
+			float newValue = currency1.convertToDollar(value);
+			result = currency2.convertFromDollar(newValue);
 		}
-
 		
-		
-		//System.out.println("---------Currency Converter---------");
-		//System.out.println("List of Available Currencies: 1 - Dollar, 2 - Real, 3 - Euro, 4 - Yen, 5 - Other");
-		//System.out.print("Enter the currency you have: ");
-		Elements tests = doc.select("a[href*=/converter/usd-bsd]");
-		for(Element test : tests) {
-			System.out.println(test.text());
-		}
-		//int selector = sc.nextInt();
-		//Currency currency1 = selectCurrency(selector);
-		//System.out.println("You have selected: " + currency1.getName());
-		
-		//System.out.print("Enter the currency you want: ");
-		//selector = sc.nextInt();
-		//Currency currency2 = selectCurrency(selector);
-		//System.out.println("You have selected: " + currency2.getName());
-		
-		//System.out.print("Enter the amount you have: ");
-		//float value = sc.nextFloat();
-		
-		//float result = 1;
-		
-		//if(currency1.getName() == "Dollar") {
-		//	result = currency2.convertFromDollar(value);
-		//} else if(currency2.getName() == "Dollar"){
-		//	result = currency1.convertToDollar(value);
-		//} else {
-		//	float newValue = currency1.convertToDollar(value);
-		//	result = currency2.convertFromDollar(newValue);
-		//}
-		
-		//System.out.println();
-		//System.out.println("Your value is worth: " + result);
+		System.out.println();
+		System.out.println("Your value is worth: " + result);
 		sc.close();
 	}	
 	
 	public static Currency selectCurrency(int aux) {
+		// initializing the HTML Document page variable 
+		Document doc; 
+		Float rate = 1.0f;
+				 
+		try { 
+			// fetching the target website 
+			doc = Jsoup.connect("https://www.exchange-rates.org/current-rates/usd").userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36") 
+							.header("Accept-Language", "*").get(); 
+		} catch (IOException e) { 
+			throw new RuntimeException(e); 
+		}
+				
 		switch(aux) {
 		case 1:
-			Currency dollar = new Currency("Dollar", "USD", 1f);
+			Currency dollar = new Currency("Dollar", "USD", rate);
 			return dollar;
 		case 2:
-			Currency real = new Currency("Real", "BRL", 5.5f);
+			rate = Float.parseFloat(doc.select("div.current-rates a[href*=/converter/usd-brl]").first().text());
+			Currency real = new Currency("Real", "BRL", rate);
 			return real;
 		case 3:
-			Currency euro = new Currency("Euro", "EUR", 1.2f);
+			rate = Float.parseFloat(doc.select("div.current-rates a[href*=/converter/usd-eur]").first().text());
+			Currency euro = new Currency("Euro", "EUR", rate);
 			return euro;
 		case 4:
-			Currency yen = new Currency("Yen", "JPY", 100.00f);
+			rate = Float.parseFloat(doc.select("div.current-rates a[href*=/converter/usd-jpy]").first().text());
+			Currency yen = new Currency("Yen", "JPY", rate);
 			return yen;
 		default:
 			Currency currency = new Currency();
